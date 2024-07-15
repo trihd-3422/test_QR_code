ca3c5314b1aa7550309e331712ab3f6696710f91
trihd-3422 <ho.duc.tri@sun-asterisk.com>
2024-07-15 14:43:31 +0700

    test qr code

---
+++ app/src/main/java/com/example/testqrcodecamera/MainActivity.kt
@@ -0,0 +1,99 @@
+package com.example.testqrcodecamera
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
+import com.example.testqrcodecamera.databinding.ActivityMainBinding
+import com.google.mlkit.vision.barcode.common.Barcode
+
+class MainActivity : AppCompatActivity() {
+
+    private val cameraPermission = android.Manifest.permission.CAMERA
+    private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+        if (isGranted) {
+            startScanner()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOpenScanner.setOnClickListener {
+            requestCameraAndStartScanner()
+        }
+    }
+
+    private fun requestCameraAndStartScanner() {
+        if (isPermissionGranted(cameraPermission)) {
+            startScanner()
+        } else {
+            requestCameraPermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        when {
+            shouldShowRequestPermissionRationale(cameraPermission) -> {
+                cameraPermissionRequest(
+                    positive = { openPermissionSetting() }
+                )
+            }
+            else -> {
+                requestPermissionLauncher.launch(cameraPermission)
+            }
+        }
+    }
+
+    private fun isPermissionGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun cameraPermissionRequest(positive: () -> Unit) {
+        AlertDialog.Builder(this).apply {
+            setTitle("Camera Permission Needed")
+            setMessage("This app needs the camera to scan QR codes. Please grant the permission.")
+            setPositiveButton("Settings") { _, _ -> positive() }
+            setNegativeButton("Cancel", null)
+            create()
+            show()
+        }
+    }
+
+    private fun openPermissionSetting() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+            data = Uri.fromParts("package", packageName, null)
+        }
+        startActivity(intent)
+    }
+
+    private fun startScanner() {
+        ScannerActivity.startScanner(this) { barcodes ->
+            barcodes.forEach { barcode ->
+                when (barcode.valueType) {
+                    Barcode.TYPE_URL -> {
+                        binding.textViewQrType.text = "URL"
+                        binding.textViewQrContent.text = barcode.url.toString()
+                    }
+                    Barcode.TYPE_CONTACT_INFO -> {
+                        binding.textViewQrType.text = "Contact"
+                        binding.textViewQrContent.text = barcode.contactInfo.toString()
+                    }
+                    else -> {
+                        binding.textViewQrType.text = "Other"
+                        binding.textViewQrContent.text = barcode.rawValue.toString()
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/testqrcodecamera/ScannerActivity.kt
@@ -0,0 +1,131 @@
+package com.example.testqrcodecamera
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.annotation.OptIn
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.testqrcodecamera.databinding.ActivityScannerBinding
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.ExecutionException
+import java.util.concurrent.Executors
+
+class ScannerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityScannerBinding
+
+    private lateinit var cameraSelector: CameraSelector
+    private lateinit var cameraProviderFuture: ListenableFuture<ProcessCameraProvider>
+    private lateinit var processCameraProvider: ProcessCameraProvider
+    private lateinit var cameraPreview: Preview
+    private lateinit var imageAnalysis: ImageAnalysis
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityScannerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        cameraSelector = CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(
+            {
+                try {
+                    processCameraProvider = cameraProviderFuture.get()
+                    bindCameraPreview()
+                    bindInputAnalyser()
+                } catch (e: ExecutionException) {
+                    Log.e(TAG, "Unhandled exception", e)
+                } catch (e: InterruptedException) {
+                    Log.e(TAG, "Unhandled exception", e)
+                }
+            }, ContextCompat.getMainExecutor(this)
+        )
+    }
+
+    private fun bindCameraPreview() {
+        cameraPreview = Preview.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .build()
+        cameraPreview.setSurfaceProvider(binding.previewView.surfaceProvider)
+        try {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, cameraPreview)
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    private fun bindInputAnalyser() {
+        val barcodeScanner: BarcodeScanner = BarcodeScanning.getClient(
+            BarcodeScannerOptions.Builder()
+                .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+                .build()
+        )
+        imageAnalysis = ImageAnalysis.Builder()
+            .setTargetRotation(binding.previewView.display.rotation)
+            .build()
+
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+
+        imageAnalysis.setAnalyzer(cameraExecutor) { imageProxy ->
+            processImageProxy(barcodeScanner, imageProxy)
+        }
+
+        try {
+            processCameraProvider.bindToLifecycle(this, cameraSelector, imageAnalysis)
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    @OptIn(ExperimentalGetImage::class)
+    private fun processImageProxy(
+        barcodeScanner: BarcodeScanner,
+        imageProxy: ImageProxy
+    ) {
+        val inputImage =
+            InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+
+        barcodeScanner.process(inputImage)
+            .addOnSuccessListener { barcodes ->
+                if (barcodes.isNotEmpty()) {
+                    onScan?.invoke(barcodes)
+                    onScan = null
+                    finish()
+                }
+            }
+            .addOnFailureListener {
+                Log.e(TAG, it.message ?: it.toString())
+            }.addOnCompleteListener {
+                imageProxy.close()
+            }
+    }
+
+    companion object {
+        private val TAG = ScannerActivity::class.simpleName
+        private var onScan: ((barcodes: List<Barcode>) -> Unit)? = null
+
+        fun startScanner(context: Context, onScan: (barcodes: List<Barcode>) -> Unit) {
+            this.onScan = onScan
+            Intent(context, ScannerActivity::class.java).also {
+                context.startActivity(it)
+            }
+        }
+    }
+}
